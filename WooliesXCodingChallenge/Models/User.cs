cd2dfb2d80b9549a3a8a8dbc57779d70fd86d6f4
samuelzheng11 <samuelzheng11@gmail.com>
2020-09-29 14:26:57 +1000

    [JOB_NUMBER] Set-up ASP.NET Core application, Remove default controllers and model, add users controller and model, add access for azure keyvault

---
+++ WooliesXCodingChallenge/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WooliesXCodingChallenge.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+        public string token { get; set; }
+    }
+}
